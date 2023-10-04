efad0d6f2a2f71f465827ab586d8317e1a66cf9b
Morten Bjerg Gregersen <759680+MortenGregersen@users.noreply.github.com>
2023-10-04 13:02:53 +0200

    Initial Commit

---
+++ AlternativeIcons/AlternativeIconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlternativeIconsApp.swift
+//  AlternativeIcons
+//
+//  Created by Morten Bjerg Gregersen on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlternativeIconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlternativeIcons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlternativeIcons
+//
+//  Created by Morten Bjerg Gregersen on 04/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
